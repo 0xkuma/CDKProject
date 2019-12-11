c0ae7a155daf84af7ac0b310c7b40212ff278e9e
Tony_K2300 <41898232+TKWS239@users.noreply.github.com>
2019-12-11 16:27:44 +0800

    Update cdk-workshop.ts
    add commit

--- bin/cdk-workshop.ts
+++ bin/cdk-workshop.ts
@@ -4,6 +4,7 @@ import cdk = require("@aws-cdk/core");
 // import { CdkWorkshopStack } from '../lib/cdk-workshop-stack';
 import { CdkWorkshopStack } from "../lib/cdk-workshop-stack";
 
+//Build stack for us-east-1
 const app = new cdk.App();
 new CdkWorkshopStack(app, "dev", {
   env: {
